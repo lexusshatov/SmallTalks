9d655dcc1227f9f365be455e02481293a39831fe
Native Developer <eugene.bychkailo.natife@gmail.com>
2021-08-12 11:15:56 +0300

    app -> core-domain

--- core/domain/src/main/java/com/natife/example/domain/di/decorator/DecoratorBindsModule.kt
+++ core/domain/src/main/java/com/natife/example/domain/di/decorator/DecoratorBindsModule.kt
@@ -4,6 +4,7 @@ import com.example.core.base.authorization.AuthorizationContract
 import com.example.core.base.chat.ChatContract
 import com.example.core.base.repository.local.PreferencesData
 import com.example.core.base.userlist.UsersContract
+import com.example.core.di.Internal
 import com.natife.example.domain.decorator.RepositoryDecorator
 import dagger.Binds
 import dagger.Module
@@ -17,17 +18,21 @@ interface DecoratorBindsModule {
 
     @Binds
     @ActivityRetainedScoped
+    @Internal
     fun bindAuthDecorator(repositoryDecorator: RepositoryDecorator): AuthorizationContract
 
     @Binds
     @ActivityRetainedScoped
+    @Internal
     fun bindChatDecorator(repositoryDecorator: RepositoryDecorator): ChatContract
 
     @Binds
     @ActivityRetainedScoped
+    @Internal
     fun bindUserListDecorator(repositoryDecorator: RepositoryDecorator): UsersContract
 
     @Binds
     @ActivityRetainedScoped
+    @Internal
     fun bindPreferencesDecorator(repositoryDecorator: RepositoryDecorator): PreferencesData
 }
--- core/src/main/java/com/example/core/di/DecoratorProvider.kt
+++ core/src/main/java/com/example/core/di/DecoratorProvider.kt
@@ -1,29 +1,50 @@
 package com.example.core.di
 
+import com.example.core.base.authorization.AuthorizationContract
+import com.example.core.base.chat.ChatContract
+import com.example.core.base.repository.local.PreferencesData
+import com.example.core.base.userlist.UsersContract
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
 import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
 class DecoratorProvider {
-    private val injector = DecoratorInjector()
+/*
+    @Inject
+    @Internal
+    lateinit var authDecorator: AuthorizationContract
+
+    @Inject
+    @Internal
+    lateinit var chatDecorator: ChatContract
+
+    @Inject
+    @Internal
+    lateinit var usersDecorator: UsersContract
+
+    @Inject
+    @Internal
+    lateinit var preferencesData: PreferencesData
+*/
 
     @Provides
     @ActivityRetainedScoped
-    fun provideAuthDecorator() = injector.authDecorator
+    fun provideAuthDecorator(@Internal authDecorator: AuthorizationContract) = authDecorator
 
     @Provides
     @ActivityRetainedScoped
-    fun provideChatDecorator() = injector.chatDecorator
+    fun provideChatDecorator(@Internal chatDecorator: ChatContract) = chatDecorator
 
     @Provides
     @ActivityRetainedScoped
-    fun provideUsersDecorator() = injector.usersDecorator
+    fun provideUsersDecorator(@Internal usersDecorator: UsersContract) = usersDecorator
 
     @Provides
     @ActivityRetainedScoped
-    fun providePreferencesDecorator() = injector.preferencesDecorator
+    fun providePreferencesDecorator(@Internal preferencesData: PreferencesData) = preferencesData
 }
